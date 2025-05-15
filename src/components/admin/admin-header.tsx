6a40f8ba0f9d228cf7dd7a2c40ecfbf8e084a897
Przemysław Kitowski <kitowski.przemek@gmail.com>
2025-05-15 23:11:58 +0200

    Add z-index

--- src/components/admin/admin-header.tsx
+++ src/components/admin/admin-header.tsx
@@ -3,7 +3,7 @@ import { SidebarTrigger } from "../ui/sidebar";
 
 export default function AdminHeader() {
   return (
-    <header className="bg-background sticky top-0 flex h-14 items-center gap-4 border-b pr-4 sm:pr-6">
+    <header className="bg-background z-1 sticky top-0 flex h-14 items-center gap-4 border-b pr-4 sm:pr-6">
       <SidebarTrigger className="rounded-t-none rounded-b-none" />
 
       <div className="ml-auto flex items-center gap-2 sm:gap-4">
